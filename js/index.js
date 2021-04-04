aaa871f8eb4c0db36f0ab367e59c6ded5c15c0af
BENSIDHOUM Azzedine <azzedine.bensidhoum@outlook.com>
2021-04-04 16:57:31 +0200

    envoi des fichiers manquants du dossier

--- js/index.js
+++ js/index.js
@@ -4,6 +4,7 @@ import "../css/style.css"
 const sideBar = document.querySelector(".edit")
 const closeButton = document.querySelector(".close")
 const openLink = document.querySelector(".open-link")
+const cancelButton = document.querySelector(".cancel-button")
 
 const close = () => {
 	sideBar.classList.remove("open-link")
@@ -15,6 +16,7 @@ const open = () => {
 
 closeButton.addEventListener("click", close)
 openLink.addEventListener("click", open)
+cancelButton.addEventListener("click", close)
 
 
 
