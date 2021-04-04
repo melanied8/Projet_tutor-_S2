b3e48cd1696b7f51e6fe864cbb143701a75b68e1
BENSIDHOUM Azzedine <azzedine.bensidhoum@outlook.com>
2021-04-04 16:17:13 +0200

    update index.js

--- js/index.js
+++ js/index.js
@@ -3,12 +3,18 @@ import "../css/style.css"
 
 const sideBar = document.querySelector(".edit")
 const closeButton = document.querySelector(".close")
+const openLink = document.querySelector(".open-link")
 
 const close = () => {
-	sideBar.classList.add("open")
+	sideBar.classList.remove("open-link")
+}
+
+const open = () => {
+	sideBar.classList.add("open-link")
 }
 
 closeButton.addEventListener("click", close)
+openLink.addEventListener("click", open)
 
 
 
