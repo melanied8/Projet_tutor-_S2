845b52cabff7cc689d2ee4939887ff248b64e928
BENSIDHOUM Azzedine <azzedine.bensidhoum@outlook.com>
2021-04-04 11:32:27 +0200

    ajout du fichier index.js et dynamise sidebar a droite

--- js/index.js
+++ js/index.js
@@ -1,6 +1,14 @@
-import "./style.css"
+import "../css/style.css"
 
 
+const sideBar = document.querySelector(".edit")
+const closeButton = document.querySelector(".close")
+
+const close = () => {
+	sideBar.classList.add("open")
+}
+
+closeButton.addEventListener("click", close)
 
 
 
