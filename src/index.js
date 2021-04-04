4e0b3228bdd55011d36335d9c48d2513df23c14e
BENSIDHOUM Azzedine <azzedine.bensidhoum@outlook.com>
2021-04-04 16:04:25 +0200

    ajout dossier src manquant

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import "./style.css";
+
+
+const sideBar = document.querySelector(".edit");
+const closeButton = document.querySelector(".close");
+
+const close = () => {
+	sideBar.classList.add("open");
+};
+
+closeButton.addEventListener("click", close);
+
+
+
+
