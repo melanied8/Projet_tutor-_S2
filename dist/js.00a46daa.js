1c765a1030bbaa0680d14739b8a676a3d1efcdca
Mélanie Dang <melanie.dang8@gmail.com>
2021-04-07 08:46:53 +0200

    modifs barre gauche

--- dist/js.00a46daa.js
+++ dist/js.00a46daa.js
@@ -410,7 +410,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "62410" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "62504" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
