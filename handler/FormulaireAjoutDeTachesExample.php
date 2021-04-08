8cecee2ff6989602c5d07b7860b53a171f1152f0
Nadia <71870432+nadiaaboudias@users.noreply.github.com>
2021-04-08 19:19:19 +0200

    Add files via upload

---
+++ handler/FormulaireAjoutDeTachesExample.php
@@ -0,0 +1,35 @@
+<?php
+	//Initialise the session
+	require('../index.php');
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="./css/style.css" />
+	<link rel="preconnect" href="https://fonts.gstatic.com">
+	<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500&display=swap" rel="stylesheet">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0"
+  	/>
+	<title>Connexion</title>
+</head>
+<body>
+<?php echo $_SESSION["email"] ?> 
+	<div class="login-wrapper">
+	<form action="<?= route('/processAddTask') ?>" method="POST" class="login">
+		<label>Nom de la liste</label> 
+		<input class="box-model" type="text" name="description" placeholder="la tache">
+        <input class="box-model" type="date" name="deadline" placeholder="la date">
+		<button class="button box-model pink" type="submit">ajouter</button>
+	</form>
+
+    <?php if (!empty($_SESSION["msg_addTask"]))
+            { 
+             echo $_SESSION["msg_addTask"]; 
+            } 
+        ?>
+	</div>
+
+</body>
+</html>
---
+++ handler/processAddTask.php
@@ -0,0 +1,38 @@
+<?php
+	//Initialise the session
+	require('../index.php');
+	//To remove the notices
+	error_reporting(E_ALL ^ E_NOTICE);
+
+    if ($_SERVER['REQUEST_METHOD']==="POST")
+    {
+        if (isset($_POST['description']) )
+        {  
+                //on réccupère l'id qui correspond à la liste ouverte 
+                $leNomDeLaList = "test";
+                $sql = $db->prepare("SELECT idList FROM list WHERE name=? LIMIT 1");
+                $sql->execute([$leNomDeLaList]);
+                $result = $sql-> fetch();
+                //echo $result[0];
+                $idList =  $result[0];
+
+    
+                //on ajoute le nom de la liste à la base de donnée
+                $request = $db->prepare("INSERT INTO listitems (description, deadline,  idList)
+                    VALUES(:description, :deadline :idList)");
+    
+                    //the parameters are bind to a specific variable name
+                    $description = $_POST['description'];
+                    $date = $_POST['deadline'];
+                    $request->bindParam(':description', $description);
+                    $request->bindParam(':deadline', $date);
+                    $request->bindParam(':idList', $idList);
+                    $request->execute();
+
+                    $_SESSION["msg_addList"] = "Nouvelle tache a été ajoutée avec succés";
+
+                    header("Location: FormulaireAjoutDeTachesExample.php");
+                    exit();   
+        }
+    }
+?>
