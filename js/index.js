bf17564df9ec82b584f8c1e2d9fdd2a2e8c1c8fd
Mélanie Dang <72560130+melanied8@users.noreply.github.com>
2021-04-08 15:37:34 +0200

    Update index.js

--- js/index.js
+++ js/index.js
@@ -19,3 +19,48 @@ closeButton.addEventListener("click", close)
 cancelButton.addEventListener("click", close)
 
 
+
+/*Barre de menu à gauche*/
+
+const openMenuButton = document.querySelector(".menu-open-button")
+const closeMenuButton = document.querySelector(".menu-close-button")
+const menu = document.querySelector(".sidebar")
+const wrapper = document.querySelector(".wrapper")
+const newList = document.querySelector(".new-list")
+let writeList = document.getElementById("write-new-list");
+
+const openMenu = () => {
+  menu.classList.add("menu-open")
+  wrapper.classList.add("background_dark")
+}
+
+if(openMenuButton)
+{
+  openMenuButton.addEventListener("click", openMenu)
+}
+
+
+const closeMenu = () => {
+  menu.classList.remove("menu-open")
+  wrapper.classList.remove("background_dark")
+  
+}
+
+  if(closeMenuButton)
+  {
+    closeMenuButton.addEventListener("click", closeMenu)
+  }
+
+
+  const writeNewList = () => {
+    if(getComputedStyle(writeList).display != "none")
+    {
+      writeList.style.display = "none";
+    }
+    else 
+    {
+      writeList.style.display = "block";
+    }
+  }
+
+  newList.addEventListener("click",writeNewList)
