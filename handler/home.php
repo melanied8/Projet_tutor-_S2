078582774fd09be719842e570fb9659a845d2f80
Mélanie Dang <72560130+melanied8@users.noreply.github.com>
2021-04-10 00:18:09 +0200

    Update home.php

--- handler/home.php
+++ handler/home.php
@@ -17,34 +17,10 @@
 	</head>
 	<body>
 
-		<div class="wrapper">
-			<div class="sidebar">
-				<div class="home">
-					<img src="./assets/home.svg"> 
-					<span>
-						<?php echo $_SESSION["email"] ?>		
-					</span>	
-				</div> 
-				<div class = "lists_and_line">
-					<h1>Mes listes</h1>
-					<svg class="line" width="176" height="1" viewBox="0 0 176 1" fill="none" xmlns="http://www.w3.org/2000/svg">
-						<line x1="4.37114e-08" y1="0.5" x2="176" y2="0.500015" stroke="#ADADAD"/>
-					</svg>
-				</div>
-		
-				<ul class="nav-list my-lists">
-					<li class="nav-list-item">To do today<div class="increment-box">2</div></li>
-					<li class="nav-list-item">Stage<div></div></li>
-					<li class="new-list"><button type="submit" class="list-button"><img class="plus-pink" src="./assets/plus-pink.svg"></button><input type="text" name="new-list" placeholder="Nouvelle liste"></li>
-				</ul>
+	<div class="wrapper">
+
 			
-				<div class = "settings_and_deco">
-					<img src="./assets/settings.svg">
-					<a href="settings.php">Paramètres</a> <!-- je sais pas pk qd je met le router ça m'envoie sur un .html -->
-					<img src="./assets/logout.svg">
-					<a href="<?= route('/logout') ?>">Déconnexion</a>
-				</div>
-			</div>
+			<?php include('sidebar_menu.php'); ?>
 
 
 			<!---
@@ -58,23 +34,23 @@
 				<li></li>
 			</ul>
 			</div>
-		-->
+			-->
 	   
 		
 
-			<div class="desktop-grid">
+		<div class="desktop-grid">
 				
-				<div class = "rectangle_settings">
-					<button type="button" class="menu-open-button"></button>
-					<p>Prochaines tâches</p>
-				</div>
+			<div class = "rectangle_settings">
+			<button type="button" class="menu-open-button"></button>
+			<p>Prochaines tâches</p>
+		</div>
 
-			</div>
+		</div>
 
-	    </div>
+	</div>
 
 
 
-		<script src="./dist/app.js"></script>
+	<script type="module" src="./dist/app.js"></script>
 	</body>
 </html>
