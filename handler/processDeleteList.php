da7b17d1e8360e6cfce3c765e3897b236a73f50f
Mélanie Dang <72560130+melanied8@users.noreply.github.com>
2021-04-16 01:20:02 +0200

    Delete handler directory

--- handler/processDeleteList.php
+++
@@ -1,9 +0,0 @@
-<?php
-    $nameList = $_GET['name'];  
-    // sql to delete a record
-    $stmt =$db->prepare("DELETE FROM list WHERE name= :name");
-    $stmt->execute( [ ':name' => $nameList,]);
-    $stmt-> execute();
-
-    
-?>
