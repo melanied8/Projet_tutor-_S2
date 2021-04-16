0806c50069bb9c2bcc0b020b8ff85da077315ef1
Nadia <71870432+nadiaaboudias@users.noreply.github.com>
2021-04-16 10:51:38 +0200

    Update processDisplayList.php

--- handler/processDisplayList.php
+++ handler/processDisplayList.php
@@ -1,33 +1,34 @@
 <?php
+	//we retrieve the id of the user
 	$stmt =$db->prepare("SELECT id FROM users WHERE email= :email"); 
 	$stmt-> execute(['email' => $_SESSION["email"]]);
 	$usersId = $stmt-> fetchAll(PDO::FETCH_ASSOC);
-    if ($usersId > 0) {
-	foreach($usersId as $row) 
-    {
-		$IDuser = $row["id"];
-		
-	}
+	    if ($usersId > 0) {
+		foreach($usersId as $row) 
+	    	{
+			$IDuser = $row["id"];
+		}
 
-	$list =$db->prepare("SELECT name, idList FROM list WHERE id= $IDuser"); 
-	$list-> execute();
-	$lesLists = $list-> fetchAll(PDO::FETCH_ASSOC);
-	foreach($lesLists as $row) 
-	{ 
-        ?>
-		<li><a href ="listDetails"></li>
-        <?php 
-		$lien = route('/listDetails');
-		?>
-		<div class = "display_list">
-       <?php echo "<a href =$lien?id=" . $row["idList"] .">" . $row["name"]."</a>";  
-		?>
-		</div>
-		<?php 
-		
-    }
-	}
-	else{
-    echo "0 results";
-	}
+		//We retrieve every list name and list id of that user
+		$list =$db->prepare("SELECT name, idList FROM list WHERE id= $IDuser"); 
+		$list-> execute();
+		$lesLists = $list-> fetchAll(PDO::FETCH_ASSOC);
+		foreach($lesLists as $row) 
+		{ 
+			?>
+			<li><a href ="listDetails"></li>
+			<?php 
+			$lien = route('/listDetails');
+			?>
+			<div class = "display_list">
+		       <?php echo "<a href =$lien?id=" . $row["idList"] . ">" . $row["name"]."</a>";  
+			?>
+			</div>
+			<?php
+	   	}
+		}
+		else {
+	   	echo "0 results";
+		}
 ?>
+
