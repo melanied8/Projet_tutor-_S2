caf0660b3e94dad04d8451c75762f08a335974b3
Nadia <71870432+nadiaaboudias@users.noreply.github.com>
2021-04-16 11:03:52 +0200

    Update settings.php

--- handler/settings.php
+++ handler/settings.php
@@ -1,9 +1,3 @@
-<?php
-	//To remove the notices
-	error_reporting(E_ALL ^ E_NOTICE);
-	//To update the error indications
-    //$_SESSION["msg_change_email"]="";
-?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -17,11 +11,19 @@
   />
 </head>
 <body>
+	<?php 
+	if (!isset($_SESSION["email"])) {
+			$_SESSION["msg_connection"] = "Veuillez vous connecter.";
+			header("Location: login");
+			exit(); 
+	} 
+	//we show the home page 
+	else { ?>
 	<div class="wrapper">
 
 	<?php include('sidebar_menu.php') ?>
 
-<div class="desktop-grid">
+	<div class="desktop-grid">
 
 	<div class = "rectangle_settings">
 	<button type="button" class="menu-open-button"><img src="./assets/menu.svg"></button>
@@ -29,49 +31,60 @@
 	</div>
 
 
-<div class = "settings">
+	<div class = "settings">
 
-<!-- Change your mail form -->
-<h2>Adresse e-mail</h2>
-<p>Adresse e-mail actuelle : <b><?php echo $_SESSION["email"] ?></b>  </p> 
-<form action="<?= route('/processSettingsChangeMail') ?>" method="POST">
-<label>Nouvelle adresse e-mail</label>
-<input class="box-model" type="email" name="email" placeholder="mail@provider.com">
-<label>Confirmer l'adresse e-mail</label>
-<input class="box-model" type="email" name="email_confirmation" placeholder="mail@provider.com">
-<button class="button box-model" type="submit">Modifier l'adresse e-mail</button>
-</form>
-<?php if (!empty($_SESSION["msg_change_email"]))
-            { 
-             echo $_SESSION["msg_change_email"]; 
-            } 
-        ?>
+	<!-- Change your mail form -->
+	<h2>Adresse e-mail</h2>
+	<p>Adresse e-mail actuelle : <b><?php echo $_SESSION["email"] ?></b>  </p> 
+	<form action="<?= route('/processSettingsChangeMail') ?>" method="POST">
+		<label>Nouvelle adresse e-mail</label>
+		<input class="box-model" type="email" name="email" placeholder="mail@provider.com">
+		<label>Confirmer l'adresse e-mail</label>
+		<input class="box-model" type="email" name="email_confirmation" placeholder="mail@provider.com">
+		<button class="button box-model" type="submit">Modifier l'adresse e-mail</button>
+		<?php 
+			//Information on the current process
+			if (!empty($_SESSION["msg_change_email"]))
+				{ 
+				echo $_SESSION["msg_change_email"]; 
+				} 
+			?>
+	</form>
 
-<!-- Change password form-->
-<h2 class = "settings-password">Mot de passe</h2>
-<form action="<?= route('/processSettingsChangePassword') ?>" method="POST">
-<label>Mot de passe actuel</label>
-<input class="box-model" type="password" name="current_password" placeholder="password">
-<label>Nouveau mot de passe</label>
-<input class="box-model" type="password" name="new_password" placeholder="password">
-<label>Confirmer le nouveau mot de passe</label>
-<input class="box-model" type="password" name="confirm_password" placeholder="password">
-<button class="button box-model" type="submit">Modifier le mot de passe</button>
-</form>
-<?php if (!empty($_SESSION["msg_change_password"]))
-            { 
-             echo $_SESSION["msg_change_password"]; 
-            } 
-        ?>
-</div>
+	<!-- Change password form-->
+	<h2 class = "settings-password">Mot de passe</h2>
+	<form action="<?= route('/processSettingsChangePassword') ?>" method="POST">
+		<label>Mot de passe actuel</label>
+		<input class="box-model" type="password" name="current_password" placeholder="password">
+		<label>Nouveau mot de passe</label>
+		<input class="box-model" type="password" name="new_password" placeholder="password">
+		<label>Confirmer le nouveau mot de passe</label>
+		<input class="box-model" type="password" name="confirm_password" placeholder="password">
+		<button class="button box-model" type="submit">Modifier le mot de passe</button>
+		<?php 
+		//Information on the current process
+		if (!empty($_SESSION["msg_change_password"]))
+			{ 
+			echo $_SESSION["msg_change_password"]; 
+			} 
+		?>
+	</form>
+	<?php if (!empty($_SESSION["msg_change_password"]))
+		    { 
+		     echo $_SESSION["msg_change_password"]; 
+		    } 
+		?>
+	</div>
 
-</div>
+	</div>
 
-</div>
+	</div>
 
-<script type="module" src="./handler/dist/sidebarList.js"></script>
-
+	<script type="module" src="./handler/dist/sidebarList.js"></script>
+	<?php } ?>
 </body>
 
 </html>
+
+
 
