3a90581a53bcdedfa5f3cb1f27cf47ac11aa40d9
Nadia <71870432+nadiaaboudias@users.noreply.github.com>
2021-04-16 10:53:35 +0200

    Update processForgottenPassword.php

--- handler/processForgottenPassword.php
+++ handler/processForgottenPassword.php
@@ -1,10 +1,7 @@
 <?php
-    //To remove the notices
-    $_SESSION["msg_reset"]="";
-
-
     if ($_SERVER['REQUEST_METHOD']==="POST")
         {
+            //we check that the field is not empty 
             if (empty($_POST['email'])) {
                 $_SESSION["msg_reset"]="Veuillez saisir une adresse e-mail.";
                 header("Location: forgottenPassword");
@@ -12,11 +9,13 @@
             }
             else
             {   
+                //we try and retrieve the email from the dataBase
                 $stmt =$db->prepare("SELECT * FROM users WHERE email= :email"); 
                 $stmt-> execute(['email' => $_POST["email"]]);
                 $users = $stmt->fetch();
 
-                if ($users===NULL)
+                //if it doesn't exist we display a message
+                if ($users==NULL)
                 {
                     $_SESSION["msg_reset"]="L'adresse mail n'existe pas.";
                     header("Location: forgottenPassword");
@@ -24,21 +23,23 @@
                 }
                 else
                 {
-                    // la fonction d'envoie d'email ne fonctionne pas 
-                    // il n'y pas encore de lien entre updatePassword et forgotPassword
-                    ini_set( 'display_errors', 1);
-                    error_reporting( E_ALL );
-                    $from = "email.ptut@gmail.com";
-                    $to = $_POST['email'];
-                    $subject = "Vérification PHP Mail";
-                    $message = "PHP mail marche";
-                    $headers = "From:" . $from;
-                    mail($to,$subject,$message, $headers);
-                    $_SESSION["msg_reset"]="L'email a été envoyé.";
-                    header("Location: forgottenPassword");
+                    // the function to send an email doesn't work  
+                    //there is no link between updatePassword.php and forgotPassword.php
+                        /* ini_set( 'display_errors', 1);
+                            error_reporting( E_ALL );
+                            $from = "email.ptut@gmail.com";
+                            $to = $_POST['email'];
+                            $subject = "Vérification PHP Mail";
+                            $message = "PHP mail marche";
+                            $headers = "From:" . $from;
+                            mail($to,$subject,$message, $headers);
+                            $_SESSION["msg_reset"]="L'email a été envoyé.";*/
+
+                    //For now the user is directly sent to de updatePassword page 
+                    $_SESSION["email"] =  $_POST["email"];
+                    header("Location: updatePassword");
                     exit();	
                 }
             }
         } 
 ?>
- 
