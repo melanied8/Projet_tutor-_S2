febeae88ff9a58e8b0421debc6d7f0face587462
Nadia <71870432+nadiaaboudias@users.noreply.github.com>
2021-04-16 10:54:11 +0200

    Update processLogin.php

--- handler/processLogin.php
+++ handler/processLogin.php
@@ -1,25 +1,27 @@
-<?php
-    
+<?php  
 if ($_SERVER['REQUEST_METHOD']==="POST")
 	{
+		//the verify that all the field are filled before sending the query 
 		if (isset($_POST['email']) && isset($_POST['password']))
 		{
+			//we retrieve the row linked to the email
 			$stmt =$db->prepare("SELECT * FROM users WHERE email= :email"); 
 			$stmt-> execute(['email' => $_POST["email"]]);
 			$users = $stmt->fetch();
+			//we verify if the password is the same as the crypted one
 			$ok = password_verify($_POST["password"], $users["password"]);
 			
+			//if not we display a message
 			if (!$ok)
 			{
-				$_SESSION["msg"] = "Le mot de passe ou l'email est incorrect";
+				$_SESSION["msg_login"] = "Le mot de passe ou l'email est incorrect";
 				header("Location: login");	
 				exit();
 			} 
 			else 
 			{
-				$_SESSION["users"] = $users;	
+				//we keep the email of the user and him to the home page 
 				$_SESSION["email"] = $_POST["email"];
-				$_SESSION["msg"] = "les informations sont valides";
 				header("Location: home");
 				exit();					
 			}	
