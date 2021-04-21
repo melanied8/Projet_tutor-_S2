3a5fbd93939675f25476f59803b98967bf7f5091
Nadia <71870432+nadiaaboudias@users.noreply.github.com>
2021-04-22 00:19:03 +0200

    Add files via upload

---
+++ handler/processDisplayTaskListDetails.php
@@ -0,0 +1,24 @@
+<!-- FOR THE HOME PAGE -->
+<?php
+
+    $listId = $_GET['id'];
+		
+            //we select all of their tasks
+			$task = $db->prepare("SELECT description FROM listitems WHERE idList = :idList"); 
+			$task->execute(['idList'=> $listId ]);
+			$lesTask  = $task-> fetchAll(PDO::FETCH_ASSOC);
+			foreach($lesTask as $row1) 
+			{
+				?>
+                <li class="nav-box flex-item item-size space-between">
+                    <div class="flex-item">
+                    <input class="radio-size radio" type="radio" name="" id="radio">   
+                    <?php	echo $row1['description'] . " "; ?> 
+                    </div>
+                    <a href=""><img class="delete" src="./assets/delete.svg"></a>
+                </li> 
+			    <?php
+			}
+		
+    
+?>
