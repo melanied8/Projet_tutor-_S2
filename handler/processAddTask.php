d4f03b78f5c1e953d66fab34fd6bf41ad553a7ce
Nadia <71870432+nadiaaboudias@users.noreply.github.com>
2021-04-22 15:57:19 +0200

    Update processAddTask.php

--- handler/processAddTask.php
+++ handler/processAddTask.php
@@ -1,25 +1,25 @@
 <?php
     //get the name of the task from addTask asynchronous js function 
-    $inputJSON = file_get_contents('php://input');
-    $input = json_decode($inputJSON, TRUE); //convert JSON into array
-    
-    //var_dump($input);
-    
-    //we retrieve the id list 
-    $idList = $_GET['id'];            
+    //$inputJSON = file_get_contents('php://input');
+    //$input = json_decode($inputJSON, TRUE); //convert JSON into array
+     //we retrieve the id list 
+     $idList = $_SESSION["idList"];
+     echo ($idList);
+       
     //we add the spot to the database
-    $request = $db->prepare("INSERT INTO listitems (description, deadline, idList)
-                             VALUES(:description, :deadline, :idList)");
+    $request = $db->prepare("INSERT INTO listitems (description, idList)
+                             VALUES(:description, :idList)");
     
     //the parameters are bind to a specific variable name
-    $description = $input;
-    $date = "12/04/2021";
+    //$description = $input;
+      
+    $description = $_POST["description"];
     $request->bindParam(':description', $description);
-    $request->bindParam(':deadline', $date);
     $request->bindParam(':idList', $idList);
     $request->execute();
 
-    header("Location: listDetails");
+    header("Location: home");
     exit(); 
   
 ?>
+
