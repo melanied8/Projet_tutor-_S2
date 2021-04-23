9917d6ab750b5b0fac1051307cd3b175906ccb0d
Mélanie Dang <72560130+melanied8@users.noreply.github.com>
2021-04-23 11:22:20 +0200

    Add files via upload
    ajout process récupération du nom de la liste

---
+++ handler/processGetNameList.php
@@ -0,0 +1,21 @@
+<?php
+$_SESSION["nameList"]=NULL;
+//Delete notices
+error_reporting(E_ALL ^ E_NOTICE);
+
+    $listId = $_GET['id'];
+    //Get the name of the list
+            $request = $db->prepare("SELECT name FROM list WHERE idList = :idList"); 
+			$request->execute(['idList'=> $listId ]);
+			$nameOfTheList = $request-> fetch(PDO::FETCH_ASSOC);
+
+            if($nameOfTheList!=NULL)
+            {
+                $_SESSION["nameList"]=$nameOfTheList['name'];
+            }
+            else
+            {
+                $_SESSION["nameList"]="Erreur dans la récupération de la liste";
+            }
+            
+?>
